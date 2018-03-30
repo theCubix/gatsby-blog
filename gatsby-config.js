a8723ca9cee615fdad2a86c7302099a9d8286a93
[thecubix] <[timon.forrer@gmail.com]>
2018-03-30 23:10:58 +0200

    Tried to add fancy images

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,25 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
-    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 650,
+              showCaptions: true
+            }
+          }
+        ]
+      }
+    },
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
