8aef15b265a4ee597af0d425814c229ce96d6e7b
[thecubix] <[timon.forrer@gmail.com]>
2018-03-30 21:42:10 +0200

    Template for blogposts

---
+++ src/templates/blogpost.jsx
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+
+class BlogPost extends Component {
+  render() {
+    const {
+      titel,
+      beschreibung
+    } = this.props.data.contentfulBlogPost
+    return (
+      <div>
+        <h1>{titel}</h1>
+        <div dangerouslySetInnerHTML={{__html: beschreibung.childMarkdownRemark.html}} />
+      </div>
+    )
+  }
+}
+
+BlogPost.propTypes = {
+  data: PropTypes.object.isRequired
+}
+
+export default BlogPost
+
+export const pageQuery = graphql`
+  query blogPostQuery($pfad: String!){
+    contentfulBlogPost(pfad: {eq: $pfad}) {
+      titel
+      pfad
+      beschreibung {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+  `
